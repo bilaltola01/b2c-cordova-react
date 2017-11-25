81075d4a530cfa4d64df8280a54593a09184a670
bilaltola01 <25662916+bilaltola01@users.noreply.github.com>
2017-11-25 13:36:00 +0000

    implemented burger menu

--- www/components/ArticleMenu.jsx
+++ www/components/ArticleMenu.jsx
@@ -4,9 +4,28 @@ import { Link } from 'react-router-dom';
 
 class ArticleMenu extends Component {
   render() {
-		const { menu, currency } = this.props;
+		const { menu, currency, languages } = this.props;
 
     const symbol = (currency && currency.Currency) ? currency.Currency.Symbol : '£';
+
+    const languagesComponent = (languages && languages.length > 0) ? languages.map((lang, index) => {
+        return (lang.Language.Flag && lang.Language.Flag.Path)
+          ? (
+            <span className="restaurant-details--language" key={index}>
+            <img src={lang.Language.Flag.Path} alt={lang.Language.Flag.AltDescription} />
+          </span>
+        ) : (index < languages.length - 1)
+            ? (
+            <span className="restaurant-details--language" key={index}>
+              {lang.Language.Name}
+              ,&nbsp;
+            </span>
+          ) : (
+            <span className="restaurant-details--language" key={index}>
+              {lang.Language.Name}
+            </span>
+          )
+      }) : null;
 
     const menuComponent = (menu.categories && menu.categories.length > 0) ? (
       <section className="meal">
@@ -25,6 +44,11 @@ class ArticleMenu extends Component {
               {menu.Description}
             </p>
           </div>
+          <div className="restaurant-details--item">
+            <h3>
+              {languagesComponent}
+            </h3>
+          </div>
         </Link>
       </section>
     ) : null;
@@ -35,7 +59,8 @@ class ArticleMenu extends Component {
 
 ArticleMenu.propTypes = {
 	menu: PropTypes.object,
-  currency: PropTypes.object
+  currency: PropTypes.object,
+  languages: PropTypes.array
 };
 
 export default ArticleMenu;
