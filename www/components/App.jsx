94b3511c9b91abd4c195e6c7a0602d6221baf37f
bilaltola01 <25662916+bilaltola01@users.noreply.github.com>
2017-12-12 11:32:20 +0000

    put back old code

--- www/components/App.jsx
+++ www/components/App.jsx
@@ -58,25 +58,17 @@ class App extends Component {
   }
 
   componentDidMount() {
-    document.addEventListener('deviceready', this.handlers.onDeviceReady, false);
-
-    if (!NavigatorUtils.isMobile(window.navigator)) {
-      this.handlers.getGeoLocation(window.navigator);
-    }
-    /*
     if (NavigatorUtils.isMobile(window.navigator)) {
       document.addEventListener('deviceready', this.handlers.onDeviceReady, false);
     } else {
       this.handlers.getGeoLocation(window.navigator);
-    }*/
+    }
   }
 
   componentWillUnmount() {
-    /*
     if (NavigatorUtils.isMobile(window.navigator)) {
       document.removeEventListener('deviceready', this.handlers.onDeviceReady, false);
     }
-    */
   }
 
   render () {
