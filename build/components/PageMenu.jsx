db6115a2bdb1626e62451a5fbd6dfc6ad81651c8
bilaltola01 <25662916+bilaltola01@users.noreply.github.com>
2019-04-20 13:17:39 -0400

    Fix #10.2 - Menu Language

--- build/components/PageMenu.jsx
+++ build/components/PageMenu.jsx
@@ -8,12 +8,23 @@ import PageContent from './PageContent';
 
 import { Pages } from './Pages';
 
+let createHandlers = (ctx) => {
+  let setMenuLanguages = (menu) => {
+    ctx.props.dispatch(actionCreators.setMenuLanguages(menu));
+  };
+
+  return {
+    setMenuLanguages
+  };
+};
+
 class PageMenu extends Component {
   constructor(props) {
     super(props);
     this.state = {
       removed: false,
     };
+    this.handlers = createHandlers(this);
   }
 
   getRestaurants(propsProfile) {
@@ -21,8 +32,9 @@ class PageMenu extends Component {
       return acc.concat(
         (current.branches || []).map(branch => {
           const finalMenus =
-            current.CompanyID === 2
+            current.CompanyID === 2 && !branch.menus
               ? ((current.branches || [])[0] || {}).menus.map(menu => {
+                  console.log(menu)
                   return {
                     ...menu,
                     MenuID: this.props.match.params.menuId,
@@ -30,7 +42,6 @@ class PageMenu extends Component {
                   };
                 })
               : branch.menus;
-
           return {
             ...branch,
             CompanyLogoPath: current.LogoPath,
@@ -46,6 +57,24 @@ class PageMenu extends Component {
     }, []);
   }
 
+  getCurrentRestaurant(restaurants, id) {
+    return (restaurants && restaurants.length > 0) ? restaurants.filter(r => {
+      return parseInt(r.BranchID, 10) === parseInt(id, 10);
+    }) : null;
+  }
+
+  getCurrentMenu(restaurant, menuId) {
+    return restaurant && restaurant[0].menus && restaurant[0].menus.length > 0
+      ? restaurant[0].menus.find(menu => {
+          return parseInt(menu.MenuID, 10) === parseInt(menuId, 10);
+        })
+      : null;
+  }
+
+  componentDidMount() {
+    this.handlers.setMenuLanguages(this.getCurrentMenu(this.getCurrentRestaurant(this.getRestaurants(this.props.profile), this.props.match.params.id), this.props.match.params.menuId));
+  }
+
   render() {
     const title = Pages.getPageAttribute('Menu', 'title');
     const footer = Pages.getPageAttribute('Menu', 'footer');
@@ -55,7 +84,6 @@ class PageMenu extends Component {
 
     let navigation = Pages.getPageAttribute('Menu', 'navigation');
     navigation.leftButtons[0].action.path += '/' + id;
-
     const restaurants = this.getRestaurants(this.props.profile);
     const restaurant =
       restaurants && restaurants.length > 0
@@ -86,7 +114,6 @@ class PageMenu extends Component {
         },
       },
     ];
-
     return (
       <div>
         <PageContent
--- build/reducers/reducers.js
+++ build/reducers/reducers.js
@@ -3,17 +3,20 @@ import * as Geolocation from './geolocation.reducer';
 import * as OffCanvas from './offcanvas.reducer';
 import * as CurrentLanguage from './current-language.reducer';
 import * as BranchLanguages from './branch-languages.reducer';
+import * as MenuLanguages from './menu-languages.reducer';
 
 const _profile = Profile._profile;
 const _geolocation = Geolocation._geolocation;
 const _offCanvas = OffCanvas._offCanvas;
 const _currentLanguage = CurrentLanguage._currentLanguage;
 const _branchLanguages = BranchLanguages._branchLanguages;
+const _menuLanguages = MenuLanguages._menuLanguages;
 
 export {
     _profile,
     _geolocation,
     _offCanvas,
     _currentLanguage,
-    _branchLanguages
+    _branchLanguages,
+    _menuLanguages
 }
